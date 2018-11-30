c825bcf226a6cf6f7e0075955f1370975e26b34c
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2018-11-29 23:02:07 -0300

    create user complete ; added firebase db

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,13 @@
-module.exports = function() {
+const admin = require('firebase-admin');
 
+module.exports = function(req, res) {
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 }
